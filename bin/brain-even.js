e058a1be9be5474a0682b2859d639ec2f2744baa
Alexander Lee <alxnder.lee@gmail.com>
2021-01-05 13:32:07 +0600

    Add brain-calc

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,34 +1,25 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 import greeting from '../src/cli.js';
+import { getRandomInt, isCorrectAnswer, isWin } from '../src/games/index.js';
 
 const userName = greeting();
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-let rightAnswersCount = 0;
+let correctAnswersCount = 0;
 
-const getRandomInt = (min, max) => {
-  const rand = min + Math.random() * (max + 1 - min);
-  return Math.floor(rand);
-};
-
-while (rightAnswersCount < 3) {
-  const number = getRandomInt(0, 100);
-  const rightAnswer = number % 2 === 0 ? 'yes' : 'no';
+while (correctAnswersCount < 3) {
+  const number = getRandomInt(0, 30);
+  const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
 
   console.log(`Question: ${number}`);
   const answer = readlineSync.question('Your answer: ');
 
-  if (answer === rightAnswer) {
-    rightAnswersCount += 1;
-    console.log('Correct!');
+  if (isCorrectAnswer(answer, correctAnswer)) {
+    correctAnswersCount += 1;
   } else {
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
-    console.log(`Let's try again, ${userName}!`);
     break;
   }
 }
 
-if (rightAnswersCount === 3) {
-  console.log(`Congratulations, ${userName}!`);
-}
+isWin(correctAnswersCount, userName);
---
+++ src/games/index.js
@@ -0,0 +1,43 @@
+export const getRandomInt = (min, max) => {
+  const rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+};
+
+export const getRandomOperation = () => {
+  const operations = ['+', '-', '*'];
+  const rand = Math.floor(Math.random() * 3);
+  return operations[rand];
+};
+
+export const calculate = (a, b, oper) => {
+  switch (oper) {
+    case '+':
+      return a + b;
+
+    case '-':
+      return a - b;
+
+    case '*':
+      return a * b;
+
+    default:
+      return null;
+  }
+};
+
+export const isCorrectAnswer = (userAnswer, correctAnswer) => {
+  if (userAnswer === correctAnswer) {
+    console.log('Correct!');
+    return true;
+  }
+  console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+  return false;
+};
+
+export const isWin = (correctAnswersCount, userName) => {
+  if (correctAnswersCount === 3) {
+    console.log(`Congratulations, ${userName}!`);
+  } else {
+    console.log(`Let's try again, ${userName}!`);
+  }
+};
